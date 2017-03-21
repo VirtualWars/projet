35c684eb1efe8ce76a3dda110c70c4d33d98b4b4
sloweed <sloweed@hotmail.fr>
2017-03-21 15:17:05 +0100

    Add files via upload
    Representation Piegeur

--- PROJETbis/src/Piegeur.java
+++ PROJETbis/src/Piegeur.java
@@ -1,62 +1,53 @@
-package projet;
+package Projet;
 
 import java.util.List;
 
-public class Piegeur extends Robot{
-	
-	public Piegeur(int equipe, int x, int y) {
-		super(equipe, x, y);
-	}
-	private static int deplacement =1;
-	private static int coutAction = -2;
-	private static int coutDeplc = 2;
-	private static int degatTir = 0;
-	private static int degatMine = -2;
-	private static String type = "Piegeur";
-	
-	
-	public static int getDeplacement() {
-		return deplacement;
-	}
-	public static void setDeplacement(int deplacement) {
-		Piegeur.deplacement = deplacement;
-	}
-	public static int getCoutAction() {
-		return coutAction;
-	}
-	public static int getCoutDeplc() {
-		return coutDeplc;
-	}
-	public int getDegatTir() {
-		return degatTir;
-	}
-	public int getDegatMine() {
-		return degatMine;
-	}
-	public String getType() {
-		return type;
-	}
-	
+public class Piegeur extends Robot {
+		private static int deplacement = 1;
+		private static int coutAction = -2;
+		private static int coutDeplc = -2;
+		private static int degatTir = 0;
+		private static int degatMine = 2;
+		private static char representation = 'P';
 		
-
-	public boolean peutTirer() {
-		return false;
-	}
-	
-	
-	public int getCoupAction() {
-		return coutAction;
-	}
-	
-	
-	public int getCoupDeplacement() {
-		return coutDeplc;
-	}
-
-	
-	public List<Coordonnee> getDeplacements() {
-		return null;
-	}
-	
-	
+		
+		public Piegeur(int equipe, int x, int y) {
+			super(equipe, x, y);
+		}
+		public static int getDeplacement() {
+			return deplacement;
+		}
+		public static void setDeplacement(int deplacement) {
+			Piegeur.deplacement = deplacement;
+		}
+		public static int getCoutAction() {
+			return coutAction;
+		}
+		public int getDegatTir() {
+			return degatTir;
+		}
+		public int getDegatMine() {
+			return degatMine;
+		}
+		public char getRepresentation() {
+			return  representation;
+		}
+		
+		@Override
+		public boolean peutTirer() {
+			// TODO Auto-generated method stub
+			return false;
+		}
+		@Override
+		public int getCoupAction() {
+			return coutAction;
+		}
+		@Override
+		public int getCoupDeplacement() {
+			return coutDeplc;
+		}
+		@Override
+		public List<Coordonnee> getDeplacements() {
+			return null;
+		}
 }
