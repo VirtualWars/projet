a4b5ed57dfe3d709768c937997a4cf6c54cd4d2a
sloweed <sloweed@hotmail.fr>
2017-03-21 15:12:40 +0100

    Add files via upload
    Representation Piegeur

---
+++ Piegeur.java
@@ -0,0 +1,58 @@
+package Projet;
+
+import java.util.List;
+
+public class Piegeur extends Robot {
+		private static int deplacement = 1;
+		private static int coutAction = -2;
+		private static int coutDeplc = -2;
+		private static int degatTir = 0;
+		private static int degatMine = 2;
+		private static char representation = 'P';
+		
+		
+		public Piegeur(int equipe, int x, int y) {
+			super(equipe, x, y);
+		}
+		public static int getDeplacement() {
+			return deplacement;
+		}
+		public static void setDeplacement(int deplacement) {
+			Piegeur.deplacement = deplacement;
+		}
+		public static int getCoutAction() {
+			return coutAction;
+		}
+		public int getDegatTir() {
+			return degatTir;
+		}
+		public int getDegatMine() {
+			return degatMine;
+		}
+		public char getRepresentation() {
+			return  representation;
+		}
+		
+		@Override
+		public boolean peutTirer() {
+			// TODO Auto-generated method stub
+			return false;
+		}
+		@Override
+		public int getCoupAction() {
+			return coutAction;
+		}
+		@Override
+		public int getCoupDeplacement() {
+			return coutDeplc;
+		}
+		@Override
+		public List<Coordonnee> getDeplacements() {
+			return null;
+		}
+		@Override
+		public char getRepresentation() {
+			// TODO Auto-generated method stub
+			return representation;
+		}
+}
